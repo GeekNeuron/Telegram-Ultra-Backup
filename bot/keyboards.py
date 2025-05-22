aac1695bd2931c8df2f8b3f9b8e0107f0a9367f7
Geek Neuron <geekneuron@gmail.com>
2025-05-22 07:00:01 +0330

    Update keyboards.py

--- bot/keyboards.py
+++ bot/keyboards.py
@@ -1,26 +1,8 @@
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from translations import load_translation
 
-def language_selection_buttons():
-    return InlineKeyboardMarkup([
-        [InlineKeyboardButton("English", callback_data="lang_en"),
-         InlineKeyboardButton("Russian", callback_data="lang_ru")],
-        [InlineKeyboardButton("Chinese", callback_data="lang_zh"),
-         InlineKeyboardButton("Hindi", callback_data="lang_hi")],
-        [InlineKeyboardButton("Spanish", callback_data="lang_es"),
-         InlineKeyboardButton("Arabic", callback_data="lang_ar")],
-        [InlineKeyboardButton("Turkish", callback_data="lang_tr"),
-         InlineKeyboardButton("Italian", callback_data="lang_it")],
-        [InlineKeyboardButton("Portuguese", callback_data="lang_pt"),
-         InlineKeyboardButton("German", callback_data="lang_de")],
-        [InlineKeyboardButton("French", callback_data="lang_fr"),
-         InlineKeyboardButton("Korean", callback_data="lang_ko")],
-        [InlineKeyboardButton("Japanese", callback_data="lang_ja"),
-         InlineKeyboardButton("Persian", callback_data="lang_fa")]
-    ])
-
-def admin_menu_buttons():
-    return InlineKeyboardMarkup([
-        [InlineKeyboardButton("Manage Backups", callback_data="admin_backups")],
-        [InlineKeyboardButton("Cloning Control", callback_data="admin_clones")],
-        [InlineKeyboardButton("Logout", callback_data="admin_logout")]
-    ])
+def main_keyboard(lang='en'):
+    t = load_translation(lang)
+    kb = ReplyKeyboardMarkup(resize_keyboard=True)
+    kb.add(KeyboardButton(t['button_send']))
+    return kb
